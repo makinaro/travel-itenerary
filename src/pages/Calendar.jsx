d166e928559f23bf82e61d5e2227eb19210fdfaf
dit <89990545+dittttt@users.noreply.github.com>
2024-11-29 23:25:49 +0800

    Fixed Calendar + Sidebar Priority Conflict

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import Fullcalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import styles from './Calendar.module.css';
 
 const Calendar = () => {
   return (
-    <div>
-      {/* Add calendar shit here */}
+    <div className={styles.calendarContainer}>
+      <Fullcalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        themeSystem="standard"
+        initialView="dayGridMonth"
+        headerToolbar={{
+          start: "prev,next today",
+          center: "title",
+          end: "dayGridMonth,timeGridWeek",
+        }}
+        buttonText={{
+          today: "This Month", // Change "Today" to "This Month"
+          month: "Month",
+          week: "Week",
+          day: "Day",
+        }}
+        height="700px" // Adjust the calendar height
+      />
     </div>
   );
 };
